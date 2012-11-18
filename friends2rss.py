03e3e2a08de34f5e61323c6736a5ab72d86ddb03
Paul Volkov <renjoki@gmail.com>
2012-11-18 13:30:18 +0400

    initial

---
+++ friends2rss.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+# This script loads LJ cookies and then generates RSS feed from friends page
+
+# Base URL
+URL = 'http://speckius.livejournal.com/friends/'
+initialURL=URL
+# How many pages to parse
+depth = 2
+
+import urllib.request
+import urllib.error
+import http.cookiejar
+import sys
+
+from bs4 import BeautifulSoup
+
+import rss_builder
+
+def open_url():
+    print('opening page', URL)
+    try:
+        return opener.open(URL)
+    except urllib.error.URLError:
+        sys.stderr.write('error while loading page\n')
+        sys.exit(2)
+
+
+def find_class(tag, searchtag, searchclass):
+    return tag.find(searchtag, {'class' : searchclass})
+
+
+def parse_page(soup, entries):
+    glob_divs = soup.findAll('div', {'class' : 'subcontent'})
+    for glob_div_tag in glob_divs:
+        entry = rss_builder.Entry()
+
+        font_tag = glob_div_tag.find('font')
+        entry.author = font_tag.contents[0]
+
+        datesubject_tag = find_class(glob_div_tag, 'div', 'datesubject')
+        date_tag = find_class(datesubject_tag, 'div', 'date')
+        entry.date = date_tag.contents[0]
+        a_tag = datesubject_tag.find('a')
+        entry.subject = a_tag.contents[0]
+        try:
+            entry.link = a_tag.attrs['href']
+        except KeyError:
+            divcomments_tag = find_class(glob_div_tag, 'div', 'comments')
+            a_tag = divcomments_tag.find('a')
+            corrected = a_tag.attrs['href'] 
+            entry.link = corrected[:corrected.find('?')]
+
+        entrytext_tag = find_class(glob_div_tag, 'div', 'entry_text')
+        entry.text = entrytext_tag.encode('utf-8').decode()
+        # need to strip <div> and </div>
+        pos = entry.text.find('>')
+        entry.text = entry.text[pos + 1:-6]
+        entries.append(entry)
+
+
+jar = http.cookiejar.MozillaCookieJar()
+try:
+    jar.load('lj_cookies.txt')
+except IOError:
+    sys.stderr.write('Error loading cookies\n')
+    sys.exit(1)
+cooker = urllib.request.HTTPCookieProcessor(jar)
+opener = urllib.request.build_opener(cooker)
+response = open_url()
+page = response.read().decode('utf-8')
+soup = BeautifulSoup(page)
+title = soup.title
+userpic_tag = soup.find('img', alt="Userpic")
+ara_tag = find_class(soup, 'a', 'i-ljuser-username')
+image = {'url' : userpic_tag.attrs['src'], 'link' : ara_tag.attrs['href'], \
+        'title' : 'image', 'width' : '100', 'height' : '100'}
+entries = []
+parse_page(soup, entries)
+depth -= 1
+while depth > 0:
+    footer_tag = find_class(soup, 'ul', 'navfooter')
+    a_tag = footer_tag.find('a')
+    try:
+        URL = a_tag.attrs['href']
+    except AttributeError:
+        sys.stderr.write("Couldn't extract next level URL")
+        break
+    response = open_url()
+    soup = BeautifulSoup(response.read().decode('utf-8'))
+    parse_page(soup, entries)
+    depth -= 1
+rss_feed = rss_builder.build_rss(entries, title, initialURL, \
+        'livejournal friends feed', image)
+with open('feed.xml', 'w') as f:
+    f.write(rss_feed)
---
+++ friends_login.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# This script prompts for LJ username and password and saves resulting cookies into a file
+
+URL = 'https://www.livejournal.com/login.bml?ret=1'
+
+import urllib.request
+import urllib.parse
+import http.cookiejar
+import ssl
+import getpass
+import sys
+
+values = { 'remember_me' : '1' }
+values['user'] = input('Username: ')
+values['password'] = getpass.getpass()
+jar = http.cookiejar.MozillaCookieJar()
+cooker = urllib.request.HTTPCookieProcessor(jar)
+https_sslv3_handler = urllib.request.HTTPSHandler(context=ssl.SSLContext(ssl.PROTOCOL_SSLv3))
+opener = urllib.request.build_opener(cooker, https_sslv3_handler)
+data = urllib.parse.urlencode(values).encode('utf-8')
+print('opening page...')
+try:
+    req = urllib.request.Request(URL, data)
+except urllib.error.URLError:
+    sys.stderr.write("Couldn't get response from server\n")
+    sys.exit(1)
+response = opener.open(req)
+print('saving cookies...')
+jar.save('lj_cookies.txt')
+respstr = response.read().decode('utf-8')
+if len(jar) == 0:
+    sys.stderr.write('Did not receive any cookies\n')
+    sys.exit(2)
+if respstr.find('<body class="logged-in" >') == -1:
+    sys.stderr.write('Response does not contain "login flags"\n')
+    sys.exit(3)
